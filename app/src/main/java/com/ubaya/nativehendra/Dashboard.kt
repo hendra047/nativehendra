121128263b4648af4ead6023514eb261c7382c89
Hendra <hdtaki04@gmail.com>
2022-02-10 23:09:40 +0700

    function getData added

--- app/src/main/java/com/ubaya/nativehendra/Dashboard.kt
+++ app/src/main/java/com/ubaya/nativehendra/Dashboard.kt
@@ -4,4 +4,8 @@ class Dashboard(var username:String) {
     override fun toString(): String {
         return "Welcome, $username"
     }
+
+    fun getData():Array<String> {
+        return arrayOf("Data 1", "Data 2")
+    }
 }
