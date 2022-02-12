35106c099c434991771ae0e00ad610879523e595
mariagracelinaa <gracezhouu@gmail.com>
2022-02-12 15:25:41 +0700

    class delete member added

---
+++ app/src/main/java/com/ubaya/nativehendra/DeleteMember.kt
@@ -0,0 +1,9 @@
+package com.ubaya.nativehendra
+
+class DeleteMember {
+    fun deleteMember(username:String): String {
+        var message = ""
+        //Masuk ke database, delete member
+        return message
+    }
+}
