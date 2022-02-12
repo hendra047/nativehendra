1107b8d98e04b5b2e4e53a36fb39731c552a94ab
mariagracelinaa <gracezhouu@gmail.com>
2022-02-12 15:16:59 +0700

    class member added

---
+++ app/src/main/java/com/ubaya/nativehendra/AddMember.kt
@@ -0,0 +1,9 @@
+package com.ubaya.nativehendra
+
+class AddMember {
+    fun addMember(name:String, age:Int): String {
+        val message = ""
+        //add ke db
+        return message
+    }
+}
