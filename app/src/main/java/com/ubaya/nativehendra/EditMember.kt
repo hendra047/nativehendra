dadea47df835447c95aaec6e83bb414eb404e48a
lenttan <s160419040@student.ubaya.ac.id>
2022-02-13 20:38:41 +0700

    edit member

---
+++ app/src/main/java/com/ubaya/nativehendra/EditMember.kt
@@ -0,0 +1,12 @@
+package com.ubaya.nativehendra
+
+class EditMember {
+    fun Edit(username:String):String{
+        var hasil = ""
+        return hasil
+    }
+
+    override fun toString(): String {
+        return "berhasil edit"
+    }
+}
